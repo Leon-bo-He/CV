cafaa2f547819a5a5c8ac9b366ba63ced19db388
Bo <bohe.ch2@gmail.com>
2022-06-28 21:38:24 -0400

    changed background videos; need text change and fix bugs

--- CV_Bo/data/content.js
+++ CV_Bo/data/content.js
@@ -5,23 +5,23 @@ export const CONTENT_INFO = {
         p: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum reiciendis inventore laborum mollitia quisquam sint quas repellat id, atque officia totam iusto vitae nemo nam ad omnis. Provident, velit excepturi."
     },
     resume: {
-        h1: "Spring",
-        h2: "Feel the new vitality",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nesciunt minima illum aliquam! Fugiat aliquam illo in et optio tenetur natus impedit voluptate cupiditate fugit aliquid, necessitatibus, veniam saepe sint."
+        h1: " ",
+        h2: " ",
+        p: " "
     },
     portfolio: {
-        h1: "Summer",
-        h2: "Enjoy passionate time",
-        p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos nam deserunt vel dolore rem expedita natus dolor accusantium accusamus tenetur, corporis a, possimus maxime recusandae? Odit repudiandae eligendi ut ea!"
+        h1: "Portfolio",
+        h2: "Exploring without limits",
+        p: "Lorem ipsum dolor. Quos nam deserunt vel dolore rem expedita natus dolor accusantium accusamus tenetur, corporis a, possimus maxime recusandae? Odit repudiandae eligendi ut ea!"
     },
     life: {
-        h1: "Autumn",
-        h2: "Experience without limits",
+        h1: "Life",
+        h2: "Change forever",
         p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, nesciunt quia. In, earum! Quae similique repellendus earum sint ratione animi veniam, libero, rerum optio, quam omnis ullam. Magni, sit illo."
     },
     contact: {
-        h1: "Winter",
-        h2: "Enjoy peach and freedom",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde necessitatibus repellat ea molestiae rerum et explicabo officiis! Quidem magnam cupiditate similique nisi mollitia nobis temporibus reprehenderit. Beatae illo aliquam soluta?"
+        h1: " ",
+        h2: " ",
+        p: " "
     },
 };
